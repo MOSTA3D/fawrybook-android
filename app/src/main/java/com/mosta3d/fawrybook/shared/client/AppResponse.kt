696473a4857d891a90f46e5f343122ae62b9bfb2
Mostafa.Esmail <mostafa.esmail@Fawry.corp>
2025-12-16 20:19:52 +0200

    [refactor] use declarative success field

--- app/src/main/java/com/mosta3d/fawrybook/shared/client/AppResponse.kt
+++ app/src/main/java/com/mosta3d/fawrybook/shared/client/AppResponse.kt
@@ -3,4 +3,7 @@ package com.mosta3d.fawrybook.shared.client
 import kotlinx.serialization.Serializable
 
 @Serializable
-data class AppResponse<T>(val data: T?, val code: String, val messages: List<String>)
+data class AppResponse<T>(val data: T?, val code: String, val messages: List<String>) {
+    val success: Boolean
+        get() = code == "0"
+}
